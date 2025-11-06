d1d0630a970a63060964353d14087a3a5566d7e3
Midra <me@midra.me>
2025-11-06 15:29:27 +0900

    xml2jsやめる

--- src/api/services/nicolog/file.ts
+++ src/api/services/nicolog/file.ts
@@ -1,43 +1,14 @@
-import type { V1Thread, V1Comment } from '@xpadev-net/niconicomments'
+import type { V1Thread } from '@xpadev-net/niconicomments'
+import type { LegacyApiXml } from '@/types/api/niconico/legacy'
 import type { GetDataFormatted } from '@/types/api/nicolog/get'
-import type { FileXml2js, ChatItemXml2js } from '@/types/api/nicolog/file'
-
-import { parseStringPromise } from 'xml2js'
 
 import { logger } from '@/utils/logger'
-import { toISOStringTz } from '@/utils/toISOStringTz'
-
-/**
- * コマンド付きコメント判定
- */
-function isCommentWithCommand(cmt: string) {
-  return /^\/[a-z_]+(?:\s|$)/.test(cmt)
-}
-
-function chatItemToV1Comment(chat: ChatItemXml2js): V1Comment {
-  const date_ms = Math.trunc(parseInt(chat.$.date) * 1000 + parseInt(chat.$.date_usec) / 1000)
-
-  return {
-    id: `${chat.$.thread}:${chat.$.no}`,
-    no: Number(chat.$.no!),
-    vposMs: Number(chat.$.vpos),
-    body: chat._,
-    commands: chat.$.mail?.split(' ') ?? [],
-    userId: chat.$.user_id!,
-    isPremium: chat.$.premium === '1',
-    score: 0,
-    postedAt: toISOStringTz(new Date(date_ms)),
-    nicoruCount: 0,
-    nicoruId: null,
-    source: 'truck',
-    isMyPost: false,
-  }
-}
+import { parseXml, legacyApiXmlToV1Thread } from '@/api/utils/niconico/legacy'
 
 export async function file<
   Result extends
     | (Compat extends true ? V1Thread : never)
-    | (Compat extends false ? FileXml2js : never),
+    | (Compat extends false ? LegacyApiXml : never),
   Compat extends boolean = false
 >(
   { name, raw_url }: GetDataFormatted,
@@ -49,22 +20,17 @@ export async function file<
     const res = await fetch(raw_url)
     const text = await res.text()
 
-    const xml: FileXml2js = await parseStringPromise(text)
+    const xml = parseXml(text)
+
+    if (!xml) {
+      return null
+    }
 
     if (options?.compatV1Thread) {
-      const comments = xml.packet.chat
-        .filter((v) => v.$.user_id && v.$.no && !isCommentWithCommand(v._))
-        .map(chatItemToV1Comment)
-      const commentCount = comments.length
-
-      const v1Thread: V1Thread = {
+      return legacyApiXmlToV1Thread(xml, {
         id: name,
         fork: 'nicolog',
-        commentCount,
-        comments,
-      }
-
-      return v1Thread as Result
+      }) as Result
     } else {
       return xml as Result
     }
---
+++ src/api/utils/niconico/legacy.ts
@@ -0,0 +1,91 @@
+import type { V1Thread, V1Comment } from '@xpadev-net/niconicomments'
+import type { LegacyApiXml, ChatItem } from '@/types/api/niconico/legacy'
+
+import { XMLParser } from 'fast-xml-parser'
+
+import { toISOStringTz } from '@/utils/toISOStringTz'
+
+const xmlParser = new XMLParser({
+  textNodeName: 'content',
+  ignoreAttributes: false,
+  attributeNamePrefix: '',
+})
+
+function isCommentWithCommand(cmt: string) {
+  return /^\/[a-z_]+(?:\s|$)/.test(cmt)
+}
+
+export function parseXml(data: string | LegacyApiXml): LegacyApiXml | null {
+  try {
+    return typeof data === 'string' ? xmlParser.parse(data) : data
+  } catch {}
+
+  return null
+}
+
+export function chatItemToV1Comment(chat: ChatItem): V1Comment {
+  const date_ms = Math.trunc(Number(chat.date) * 1000 + Number(chat.date_usec) / 1000)
+
+  return {
+    id: `${chat.thread}:${chat.no}`,
+    no: Number(chat.no!),
+    vposMs: Number(chat.vpos),
+    body: chat.content,
+    commands: chat.mail?.split(' ') ?? [],
+    userId: chat.user_id!,
+    isPremium: chat.premium === '1',
+    score: 0,
+    postedAt: toISOStringTz(new Date(date_ms)),
+    nicoruCount: 0,
+    nicoruId: null,
+    source: 'truck',
+    isMyPost: false,
+  }
+}
+
+export function legacyApiXmlToV1Thread(
+  input: string | LegacyApiXml,
+  {
+    id,
+    fork,
+  }: {
+    id: string
+    fork: string
+  }
+): V1Thread | null {
+  const xml = parseXml(input)
+
+  if (!xml) {
+    return null
+  }
+
+  try {
+    if (!Array.isArray(xml.packet.chat)) {
+      xml.packet.chat = [xml.packet.chat]
+    }
+
+    const comments = xml.packet.chat
+      .filter((chat) => {
+        return (
+          !chat.deleted &&
+          chat.no &&
+          chat.user_id &&
+          chat.content &&
+          !isCommentWithCommand(chat.content)
+        )
+      })
+      .map(chatItemToV1Comment)
+    const commentCount = comments.length
+
+    const v1Thread: V1Thread = {
+      id,
+      fork,
+      commentCount,
+      comments,
+    }
+
+    return v1Thread
+  } catch {}
+
+  return null
+}
