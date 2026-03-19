10db8e8fa8498b04053aa5b185f00cf690295873
Midra <me@midra.me>
2026-03-19 17:19:45 +0900

    seasonを含める

--- src/search/services/nicolog.ts
+++ src/search/services/nicolog.ts
@@ -1,4 +1,5 @@
 import type { ParsedResult } from '@/parse'
+import type { GetDataFormatted } from '@/types/api/nicolog/get'
 
 import { compare } from '@/compare'
 import { parse } from '@/parse'
@@ -7,7 +8,15 @@ import * as nicologApi from '@/api/services/nicolog'
 const FILE_NAME_SUFFIX_REGEXP =
   /(?:(?:振り返り)?上映会|一挙放送)(?:_raw)?\.xml$/
 
-export async function nicolog(input: string | ParsedResult) {
+const IGNORE_FILE_NAME_SUFFIXES = [
+  '_raw.xml',
+  '振り返り上映会.xml',
+  '一挙放送.xml',
+]
+
+export async function nicolog(
+  input: string | ParsedResult
+): Promise<GetDataFormatted | null> {
   const parsed = parse(input)
 
   if (!parsed.isSingleEpisode || !parsed.title || !parsed.episode) {
@@ -22,36 +31,44 @@ export async function nicolog(input: string | ParsedResult) {
     return null
   }
 
-  const base = parse(`${parsed.titleStripped} #0`)
-  const directory = directories.find((v) => compare(base, `${v.name} #0`))
-
-  if (!directory) {
-    return null
-  }
-
-  // ファイルリスト
-  const fileList = await nicologApi.list({
-    path: `${nicologApi.NICO_LIVE_ANIME_ROOT}/${directory.name}`,
-  })
-  const files = fileList?.content.filter(
-    (v) => !v.is_dir && !v.name.endsWith('_raw.xml')
-  )
-
-  if (!files?.length) {
-    return null
-  }
-
-  const file = files.find((v) =>
-    compare(parsed, v.name.replace(FILE_NAME_SUFFIX_REGEXP, ''))
-  )
-
-  if (!file) {
-    return null
-  }
-
-  const detail = await nicologApi.get({
-    path: `${nicologApi.NICO_LIVE_ANIME_ROOT}/${directory.name}/${file.name}`,
+  const base = parse(`${parsed.titleStripped} ${parsed.season?.text ?? ''} #0`)
+  const targetDirectories = directories.filter((dir) => {
+    return compare(base, `${dir.name} #0`)
   })
 
-  return detail
+  for (const directory of targetDirectories) {
+    const directoryName = directory.name.replaceAll('/', ' ')
+    const directoryPath = `${nicologApi.NICO_LIVE_ANIME_ROOT}/${directoryName}`
+
+    // ファイルリスト
+    const fileList = await nicologApi.list({
+      path: directoryPath,
+    })
+    const files = fileList?.content.filter(({ is_dir, name }) => {
+      return !is_dir && !IGNORE_FILE_NAME_SUFFIXES.some((v) => name.endsWith(v))
+    })
+
+    // ファイル
+    const file = files?.find((file) => {
+      const fileNameParsed = parse(
+        file.name.replace(FILE_NAME_SUFFIX_REGEXP, '')
+      )
+
+      // 話数だけ一致
+      return (
+        fileNameParsed.isSingleEpisode &&
+        fileNameParsed.episode?.number === parsed.episode!.number
+      )
+    })
+
+    if (!file) continue
+
+    const detail = await nicologApi.get({
+      path: `${directoryPath}/${file.name}`,
+    })
+
+    return detail
+  }
+
+  return null
 }
