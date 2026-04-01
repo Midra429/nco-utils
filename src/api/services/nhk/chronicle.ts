bde75debe406aeeed70f27b18df85a3929d8264f
Midra <me@midra.me>
2026-04-01 14:01:58 +0900

    放送日を指定できるように

--- src/api/services/nhk/chronicle.ts
+++ src/api/services/nhk/chronicle.ts
@@ -5,6 +5,9 @@ import { logger } from '@/common/logger'
 
 const API_BASE_URL = 'https://noa-api.nhk.jp/r1/db/_search'
 
+const CHANNEL_IDS_DEFAULT: NhkChannelId[] = ['21', '31', '11', '10', '44']
+const ONWARDS_DEFAULT: string = '20091128'
+
 const HALF_WIDTH_SYMBOLS_REGEXP = /[!-\/:-@\[-`\{-~]/g
 const OPEN_PUNCTUATION_REGEXP = /(?<=[^\s])(\p{Ps})/gu
 const CLOSE_PUNCTUATION_REGEXP = /(\p{Pe})(?=[^\s])/gu
@@ -13,7 +16,7 @@ function convertKeyword(keyword: string): string {
   return (
     keyword
       // 半角記号 -> 全角記号
-      .replace(HALF_WIDTH_SYMBOLS_REGEXP, function (str) {
+      .replace(HALF_WIDTH_SYMBOLS_REGEXP, (str) => {
         return String.fromCharCode(str.charCodeAt(0) + 0xfee0)
       })
       // 括弧の前後にスペースを入れる
@@ -29,17 +32,27 @@ function convertKeyword(keyword: string): string {
   )
 }
 
+export interface ChronicleParams {
+  channelIds?: NhkChannelId[]
+  onwards?: string
+}
+
 export async function chronicle(
   keyword: string,
-  channelIds: NhkChannelId[] = ['21', '31', '11', '10', '44']
+  params: ChronicleParams = {}
 ): Promise<ChronicleResult[] | null> {
+  params.channelIds ??= CHANNEL_IDS_DEFAULT
+  params.onwards = Math.max(
+    Number(ONWARDS_DEFAULT),
+    Number(params.onwards || -1)
+  ).toString()
+
   const url = new URL(API_BASE_URL)
 
   const query = [
     `(${convertKeyword(keyword)})`,
-    `channel1.keyword:(${channelIds.join(' OR ')})`,
-    // 2009/11/28以降
-    'airdate1:(>=20091128)',
+    `channel1.keyword:(${params.channelIds.join(' OR ')})`,
+    `airdate1:(>=${params.onwards})`,
   ].join(' AND ')
 
   url.searchParams.set('index', 'crn_data')
